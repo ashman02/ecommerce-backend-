4cedd28300e921fc4d7d1611bf0897c6fd905427
cforChobar <sidhuashman02@gmail.com>
2024-05-01 13:35:11 +0530

    User and Product model is done

---
+++ src/models/product.model.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: [
+    {
+      type: String, //Cloudinary url
+      required: true,
+    },
+  ],
+  price: {
+    type: Number,
+    required: true,
+  },
+
+  gender: {
+    type: String,
+  },
+
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+export const Product = mongoose.model("Product", productSchema);
