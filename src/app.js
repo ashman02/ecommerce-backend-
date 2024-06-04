6dfdca59d940703d8a11304b9080ee032445b3b8
cforChobar <sidhuashman02@gmail.com>
2024-06-04 11:27:47 +0530

    bug day 2

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const app = express()
 const allowedOrigins = ["http://localhost:5173", "https://chobarcart.netlify.app"]
 app.use(cors({
     origin : allowedOrigins,
-    methods : ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials : true
 }))
 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -112,7 +112,7 @@ const loginUser = asyncHandler(async (req, res) => {
     httpOnly: true,
     secure: true,
     sameSite : 'None',
-    domain : '.https://chobarcart.netlify.app/'
+    domain : '.netlify.app'
   };
 
   return res
